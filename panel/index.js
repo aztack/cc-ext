d9fef88272ebbdbb096d85f799344fdcea346559
wangweihua <wangweihua@vipkid.com.cn>
2020-07-28 11:20:56 +0800

    feat: add design width select

--- panel/index.js
+++ panel/index.js
@@ -1,4 +1,5 @@
 const { read, pkgName } = require_('utils.js');
+const DesignWidths = require_('design-width.js');
 
 const vm = (el) => {
   return new Vue({
@@ -44,26 +45,59 @@ Editor.Panel.extend({
 const extensions = {
   inspector: [
     function (sender) {
-      if (document.getElementById('__toggleSectionsButton')) {
-        Editor.log(`Found button, exit`);
-        return;
-      }
-      const inspector = document.querySelector('ui-panel-frame#inspector');
-      if (!inspector) {
-        Editor.log(`Can not find inspector, exit`);
-        return;
-      }
-      const btn = document.createElement('ui-button');
-      btn.id = '__toggleSectionsButton'
-      btn.innerText = $t('collapseExpandAllButton');
-      inspector.appendChild(btn);
-      inspector.shadowRoot.prepend(btn);
-      btn.onclick = () => {
-        const sections = document.querySelectorAll('ui-panel-frame#inspector::shadow ui-section .header');
-        Array.from(sections).forEach(it =>it.click());
-      };
+      createToggleAllButton();
+      createDeviceList();
     }
   ]
+}
+
+function createDeviceList() {
+  const existed = document.getElementById('__deviceList');
+  if (existed) {
+    existed.parentElement.removeChild(existed);
+  }
+  const inspector = document.querySelector('ui-panel-frame#inspector');
+  if (!inspector) {
+    Editor.log(`Can not find inspector, exit`);
+    return;
+  }
+  const select = document.createElement('ui-select');
+  select.id = '__deviceList'
+  DesignWidths.forEach(width => {
+    const opt = document.createElement('option');
+    opt.textContent = width;
+    opt.value = width;
+    select.appendChild(opt);
+  });
+
+  inspector.appendChild(select);
+  inspector.shadowRoot.prepend(select);
+  select.addEventListener('change', (e) => {
+    const designWidth = e.target.value;
+    Editor.log(`Setting design width: ${designWidth}`);
+    cc.director.getScene().children.find(s => s.name === 'Canvas').emit('set_design_width', {width: designWidth});
+  });
+}
+
+function createToggleAllButton() {
+  const existed = document.getElementById('__toggleSectionsButton')
+  if (existed) {
+    existed.parentElement.removeChild(existed);
+  }
+  const inspector = document.querySelector('ui-panel-frame#inspector');
+  if (!inspector) {
+    Editor.log(`Can not find inspector, exit`);
+    return;
+  }
+  const btn = document.createElement('ui-button');
+  btn.id = '__toggleSectionsButton'
+  btn.innerText = $t('collapseExpandAllButton');
+  inspector.appendChild(btn);
+  inspector.shadowRoot.prepend(btn);
+  btn.onclick = () => {
+    const sections = document.querySelectorAll('ui-panel-frame#inspector::shadow ui-section .header');
+    Array.from(sections).forEach(it =>it.click());
+  };
 }
 
 function require_(relativePath) {
